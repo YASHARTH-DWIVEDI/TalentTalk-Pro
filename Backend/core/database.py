3dbc56feea3fcaa144a698428a0e5e9f05f4a60e
Yasharth Dwivedi <152445741+YASHARTH-DWIVEDI@users.noreply.github.com>
2026-01-30 09:18:52 +0530

    Add files via upload

---
+++ Backend/core/database.py
@@ -0,0 +1,17 @@
+from sqlmodel import SQLModel, create_engine, Session
+from .config import get_settings
+
+settings = get_settings()
+
+engine = create_engine(
+    settings.DATABASE_URL, 
+    echo=True, 
+    connect_args={"check_same_thread": False}  # Needed for SQLite
+)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
